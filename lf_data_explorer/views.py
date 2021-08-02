430b716396b9cfcdc35d7eac9c7e3a26b31596f6
Peter Crowther <13117069+merrygoat@users.noreply.github.com>
2021-08-02 14:28:33 +0100

    Add post method

--- lf_data_explorer/views.py
+++ lf_data_explorer/views.py
@@ -1,20 +1,29 @@
 from lf_data_explorer import app
-from flask import render_template
+from flask import render_template, request
 
 from lf_data_explorer.queries import get_all_samples
 
 
 @app.route('/')
-def show_index():
+def index():
     return render_template("index.html")
 
 
 @app.route('/list_samples')
-def show_list_samples():
+def list_samples():
     samples = get_all_samples()
     return render_template('list_samples.html', samples=samples)
 
 
 @app.route('/about')
-def show_about():
+def about():
     return render_template('about.html')
+
+
+@app.route('/add_samples', methods=['POST', 'GET'])
+def add_samples():
+    if request.method == "GET":
+        return render_template('add_samples.html')
+    else:
+        new_name = request.form['sample_name']
+        return render_template('add_samples.html', sample=new_name)
